4d1bba3d409512b4564c8edcd8b03f56226d34e0
Bruno Franciosi <brunohfranciosi@gmail.com>
2022-06-08 17:45:13 -0300

    Plane

---
+++ Plane/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+    [SerializeField] private float velocity = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //taking my rb by yourself
+        myRB = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rising when pressing space
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myRB.velocity = Vector2.up * velocity;
+        }
+
+        if(myRB.velocity.y < -velocity)
+        {
+            myRB.velocity = Vector2.down * velocity;
+        }
+    }
+}
